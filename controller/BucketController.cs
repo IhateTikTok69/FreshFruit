f8b09c20b37db9c1e1d7f59d94611aecf60c5e51
IhateTikTok69 <93307842+IhateTikTok69@users.noreply.github.com>
2021-10-28 12:56:38 +0700

    Initial commit

---
+++ controller/BucketController.cs
@@ -0,0 +1,54 @@
+﻿using FreshFruit.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshFruit.controller
+{
+    class BucketController
+    {
+        private Bucket bucket;
+        private BucketEventListener eventListener;
+        private Bucket keranjangBuah;
+        private MainWindow mainWindow;
+
+        public BucketController(Bucket keranjangBuah, MainWindow mainWindow)
+        {
+            this.keranjangBuah = keranjangBuah;
+            this.mainWindow = mainWindow;
+        }
+
+        public void addFruit(Fruit fruit)
+        {
+            if (bucketIsOverloaded())
+            {
+                eventListener.onfailed("oops, keranjang penuh");
+            }
+            else
+            {
+                this.bucket.insert(fruit);
+                eventListener.onSucceed("Yay, Berhasil ditambahkan");
+            }
+        }
+
+        internal List<Fruit> findAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool bucketIsOverloaded()
+        {
+            return bucket.countItems() >= bucket.getCapacity();
+        }
+        public void removeFruit(Fruit fruit)
+        {
+            for (int itemPostition = 0; itemPostition < bucket.countItems(); itemPostition++)
+            {
+                bucket.remove(itemPostition);
+                eventListener.onSucceed("yey, berhasil dihapus");
+            }
+        }
+    }
+}
